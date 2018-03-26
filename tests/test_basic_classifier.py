86f51e59a74eb688c2fe46eb2c8b590821abed3c
David Morton <davidlmorton@gmail.com>
2018-03-26 17:03:08 -0500

    Clean up BasicClassifier

--- tests/test_basic_classifier.py
+++ tests/test_basic_classifier.py
@@ -2,39 +2,25 @@
 from sconce.data_generator import DataGenerator
 from sconce.rate_controllers import CosineRateController
 from sconce.trainers import ClassifierTrainer
-from sconce.models import basic_classifier as bc
+from sconce.models import BasicClassifier
 from torch import optim
 
+import os
 import torch
 import unittest
 
 
 class TestBasicClassifier(unittest.TestCase):
-    def test_full_run(self):
+    def _test_file(self, *relative_path):
+        base_path = os.path.dirname(__file__)
+        return os.path.join(base_path, *relative_path)
+
+    def test_full_run_from_yaml(self):
         RANDOM_SEED = 1
         torch.manual_seed(RANDOM_SEED)
 
-        keys =      ('out_channels', 'stride', 'padding', 'kernel_size')
-        values = ( # ==============  ========  =========  =============
-                    (16,             1,        4,         9),
-                    (8,              2,        1,         3),
-                    (8,              2,        1,         3),
-                    (8,              2,        1,         3),
-                    (8,              2,        1,         3),
-        )
-
-        in_channels = 1
-        convolutional_layers = []
-        for values in values:
-            kwargs = dict(zip(keys, values))
-            convolutional_layers.append(bc.ConvolutionalLayer(
-                in_channels=in_channels, **kwargs))
-            in_channels = kwargs['out_channels']
-
-        model = bc.BasicClassifier(image_width=28, image_height=28,
-                    convolutional_layers=convolutional_layers,
-                    dropouts=[0.4, 0.8],
-                    fully_connected_sizes=[100, 100])
+        filename = self._test_file('basic_classifier.yaml')
+        model = BasicClassifier.new_from_yaml_filename(filename)
 
         training_generator = DataGenerator.from_pytorch()
         test_generator = DataGenerator.from_pytorch(train=False)
